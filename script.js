13b7b2388744892da3760ff305b65f2a100921f3
128234 <128234@newington.nsw.edu.au>
2019-04-01 09:46:29 +1100

    Saves values into an array

--- script.js
+++ script.js
@@ -1,45 +1,56 @@
-alert('test');
 goToPage(page1);
-document.getElementById("lastQuestionButton").hidden = true;
+var allClassesArray = [];
+var chosenClassArray = [];
 
 //Everytime a certain button is pressed, this code will switch from page to page
 function goToPage(page) {
-  document.querySelectorAll(".pages").forEach(i => i.hidden = true);
-  page.hidden = false;
+  var pages = document.getElementsByClassName("pages");
+  for (i = 0; i < pages.length; i++) {
+    pages[i].style.display = "none";
+  }
+  page.style.display = "block";
 }
 
 //This function will add a new row to the table for the user to enter
-var studentID = 0;
-alert(studentID);
-function addNewStudent(){
-  if(studentID != 0){
-    studentID = -1;
-  } 
-  studentID += 1
-  alert(studentID)
-  table = document.getElementById("studentListTable");
-  row = table.insertRow(-1);
+var studentID = -1; //useless
+function addNewStudent() {
+  studentID += 1; //useless
+  row = document.getElementById("studentListTable").insertRow(-1);
   cell0 = row.insertCell(0);
   cell1 = row.insertCell(1);
   cell2 = row.insertCell(2);
+  
+  //This will place input elements in each cell in the table
   inputElement0 = document.createElement("input");
   inputElement1 = document.createElement("input");
   inputElement2 = document.createElement("input");
-  
-  inputElement0.id = 'fName' + studentID.toString()
-  inputElement1.id = 'sName' + studentID.toString()
-  inputElement2.id = 'rank' + studentID.toString()
-  
+  //Useless code
+  inputElement0.id = "fName" + studentID.toString();
+  inputElement1.id = "sName" + studentID.toString();
+  inputElement2.id = "rank" + studentID.toString();
+  //End of useless code
   cell0.appendChild(inputElement0);
   cell1.appendChild(inputElement1);
   cell2.appendChild(inputElement2);
 }
 
-function x() {
-    table = document.getElementById('studentListTable');
-    for (r = 1, n = table.rows.length; r < n; r++) {
-        for (c = 0, m = table.rows[r].cells.length; c < m; c++) {
-            alert(table.rows[r].cells[c].innerHTML);
-        }
-    }
+function removeLastStudent() {
+  if(document.getElementById("studentListTable").rows.length > 1){
+  document.getElementById("studentListTable").deleteRow(-1);
+  }
+}
+
+
+function saveClass() {
+  table = document.getElementById("studentListTable");
+  for (r = 1; r < table.rows.length; r++) {
+    //for (c = 0, m = table.rows[r].cells.length; c < m; c++) {
+    studentDetailsObject = {
+      firstName: table.rows[r].cells[0].childNodes[0].value,
+      lastName: table.rows[r].cells[1].childNodes[0].value,
+      rank: table.rows[r].cells[2].childNodes[0].value
+    };
+    chosenClassArray.push(studentDetailsObject);
+    //}
+  }
 }
